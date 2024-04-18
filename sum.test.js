c05d3c17a6aaa8d7bd753218e3ee021a557c526b
Junior Hirwa <iamhirwejr@gmail.com>
2024-04-18 09:28:52 +0200

    chore: initial commit

---
+++ sum.test.js
@@ -0,0 +1,8 @@
+// each unit test should be inside of a test block that starts with it or test
+/**
+ * the test block has a structure 
+   it('description of what the test block does', () => {
+    // a callback function that contains the tests, arrangements, actions and assertions.
+    // i.e test logics
+   })
+ */
