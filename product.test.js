8f444f290797b287d3013a0f89f9e64cbd5e8cf3
Junior Hirwa <iamhirwejr@gmail.com>
2024-04-22 12:25:06 +0200

    test: test the product module

---
+++ product.test.js
@@ -0,0 +1,7 @@
+const multiply = require('./product')
+
+describe('testing the outcomes of positive scenarios', () => {
+    test('test if 2 * 3 equals 6', () => {
+        expect(multiply(2, 3)).toEqual(6)
+    })
+})
