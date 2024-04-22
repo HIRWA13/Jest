abd4089e380cf26da0939d62b4dee7d7c0fe2388
Junior Hirwa <iamhirwejr@gmail.com>
2024-04-22 12:36:25 +0200

    test: change toEqual to toBE

--- product.test.js
+++ product.test.js
@@ -2,6 +2,6 @@ const multiply = require('./product')
 
 describe('testing the outcomes of positive scenarios', () => {
     test('test if 2 * 3 equals 6', () => {
-        expect(multiply(2, 3)).toEqual(6)
+        expect(multiply(2, 3)).toBE(6)
     })
 })
